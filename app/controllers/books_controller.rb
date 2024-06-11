cabd54673f5edd1f3de060ef1e6fe925b05b3dd9
kanta-program-account <151516153+kanta-program-account@users.noreply.github.com>
2024-06-11 16:57:52 +0000

    [Finish] books_controller CRUD実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
+    @user = User.find(current_user.id)
   end
 
   def index
@@ -12,6 +14,22 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    redirect_to book_path(@book.id)
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
   
