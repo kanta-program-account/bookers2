ff08cc1aa8539c5c63bcbc04d2af123ab6b7ede1
kanta-program-account <151516153+kanta-program-account@users.noreply.github.com>
2024-06-13 11:48:44 +0000

    [Finish] レイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,29 @@
 class UsersController < ApplicationController
   def show
+    @user_self = User.find(current_user.id)
+    @user_selfothers = User.find(params[:id])
+    @book = Book.new
+  end
+  
+  def index
+    @user = User.find(current_user.id)
+    @users = User.all
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(current_user.id)
+  end
+  
+  def update
+    user = User.find(current_user.id)
+    user.update(user_params)
+    redirect_to user_path(current_user.id)
   end
 
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 end
